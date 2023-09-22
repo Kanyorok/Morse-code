a7bae82cb47afccb79c9036593ef9ebd7d8aba8f
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-09-22 13:01:26 +0300

    Fix the methods in the data

--- mosecode.rb
+++ mosecode.rb
@@ -13,14 +13,22 @@ def morse_decipher(morse)
   morse_code[morse]
 end
 
+def decode_char(morse_char)
+  morse_decipher(morse_char)
+end
+
+def decode_word(morse_word)
+  morse_word.split(' ').map do |char|
+    decode_char(char)
+  end.join('')
+end
+
 def decode(morse_code)
   morse_code = morse_code.strip # Remove leading and trailing spaces
   morse_code_words = morse_code.split('   ') # Split the Morse code into words
 
   morse_code_words.map do |word|
-    word.split(' ').map do |char|
-      morse_decipher(char)
-    end.join('')
+    decode_word(word)
   end.join(' ')
 end
 
